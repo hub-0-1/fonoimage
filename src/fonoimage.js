7c15a6279cef93c8e5eeb36f72d93d8259ee9bb8
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-03-08 22:48:16 -0500

    initial

--- src/fonoimage.js
+++ src/fonoimage.js
@@ -5,6 +5,8 @@ const Fabric = require("fabric").fabric;
 import './style.less';
 import Image from './images/image.svg';
 import Ellipse from './images/ellipse.svg';
+import Record from './images/record.svg';
+import Micro from './images/micro.svg';
 
 import VueI18n from 'vue-i18n';
 import i18n from './traductions.js';
@@ -25,6 +27,49 @@ window.Fonoimage = class Fonoimage {
       methods: {
         exporter: function () {
           console.log(JSON.stringify(this.canva));
+        },
+        dessiner_nouvelle_zone: function (options) {
+          this.mode = "ajout:encours";
+
+          // Initialisation des variables de scope
+          let init_event = options.e;
+          let coords = [{
+            x: options.absolutePointer.x,
+            y: options.absolutePointer.y
+          }];
+          let shadow_style = this.$refs.shadow.style;
+
+          // Affichage shadow
+          shadow_style.left = options.e.clientX + "px";
+          shadow_style.top = options.e.clientY + "px";
+          shadow_style.width = 0;
+          shadow_style.height = 0;
+
+          // Creer a la fin du drag
+          this.canva.on('mouse:up', (options) => {
+            this.canva.off('mouse:move');
+            this.canva.off('mouse:up');
+
+            coords.push({
+              x: options.absolutePointer.x,
+              y: options.absolutePointer.y
+            })
+
+            this.ajouter_zone(
+              Math.min(coords[0].x, coords[1].x), // x
+              Math.min(coords[0].y, coords[1].y), // y
+              Math.abs(coords[0].x - coords[1].x) / 2, // width
+              Math.abs(coords[0].y - coords[1].y) / 2// height
+            );
+
+            this.mode = "normal";
+          });
+
+          // Afficher le shadow
+          this.canva.on('mouse:move', (options) => {
+            shadow_style.width = (options.e.clientX - init_event.clientX) + "px";
+            shadow_style.height = (options.e.clientY - init_event.clientY) + "px";
+          });
         },
         ajouter_zone: function (x, y, w, h) {
           let nouvelle_zone = {id: `zone-${Date.now()}-${Math.round(Math.random() * 50)}`};
@@ -51,23 +96,54 @@ window.Fonoimage = class Fonoimage {
         afficher_fonofone: function (zone_active) {
           this.zone_actif = zone_active;
 
-          // Cacher les autres zones
+          // Cacher et afficher les zones
           _.each(this.zones, (zone) => { zone.container_fonofone.style.display = "none"; });
-
-          // Afficher la zone selectionnee
           this.zone_actif.container_fonofone.style.display = "initial";
+        },
+        toggle_session: function () {
+          this.mode.match(/session/) ? this.fin_session() : this.debut_session();
+        },
+        debut_session: function () {
+          this.mode = "session:active";
+          new Fabric.Image.fromURL(Micro, (micro) => {
+            console.log(micro);
+            this.micro = micro;
+            micro.originX = "center";
+            micro.set('left', this.canva.width / 2);
+            micro.set('top', this.canva.height / 2);
+            micro.setCoords();
+            micro.on('mousedown', function (options_down) {
+
+              // TODO activer l'ecoute
+              micro.on('mouseup', function () {
+                micro.off('mousemove');
+                micro.off('mouseup');
+              });
+              micro.on('mousemove', function (options_move) {
+                // 
+                console.log(options_move);
+              })
+            });
+
+            // Empecher le resize
+            micro.hasControls = false;
+            this.canva.add(micro);
+          });
+        },
+        fin_session: function () {
+          this.mode = "normal";
+          this.canva.remove(this.micro);
+          this.micro = null;
         }
       },
-      computed: { },
       mounted: function () {
         let application = this.$refs.application_fonoimage;
 
+        // Creer le canva
         this.canva = new Fabric.Canvas('canva-fonoimage', {
           width: application.offsetWidth,
           height: application.offsetHeight
-        });
-
-        this.canva.on('mouse:down', (options) => {
+        }).on('mouse:down', (options) => {
 
           // Si on ne clique pas sur une zone
           if(!options.target) { 
@@ -76,46 +152,7 @@ window.Fonoimage = class Fonoimage {
             this.zone_actif = null; 
 
             // Creation d'une nouvelle zone
-            if(this.mode == "ajout:pret") {
-              this.mode = "ajout:encours";
-              let init_event = options.e;
-              let coords = [{
-                x: options.absolutePointer.x,
-                y: options.absolutePointer.y
-              }];
-
-              // Affichage shadow
-              this.$refs.shadow.style.left = options.e.clientX + "px";
-              this.$refs.shadow.style.top = options.e.clientY + "px";
-              this.$refs.shadow.style.width = 0;
-              this.$refs.shadow.style.height = 0;
-
-              // Creer a la fin du drag
-              this.canva.on('mouse:up', (options) => {
-                this.canva.off('mouse:move');
-                this.canva.off('mouse:up');
-
-                coords.push({
-                  x: options.absolutePointer.x,
-                  y: options.absolutePointer.y
-                })
-
-                this.ajouter_zone(
-                  Math.min(coords[0].x, coords[1].x), // x
-                  Math.min(coords[0].y, coords[1].y), // y
-                  Math.abs(coords[0].x - coords[1].x) / 2, // width
-                  Math.abs(coords[0].y - coords[1].y) / 2// height
-                );
-
-                this.mode = "normal";
-              });
-
-              // Afficher le shadow
-              this.canva.on('mouse:move', (options) => {
-                this.$refs.shadow.style.width = (options.e.clientX - init_event.clientX) + "px";
-                this.$refs.shadow.style.height = (options.e.clientY - init_event.clientY) + "px";
-              });
-            }
+            if(this.mode == "ajout:pret") { this.dessiner_nouvelle_zone(options); }
           }
         });
       },
@@ -126,11 +163,14 @@ window.Fonoimage = class Fonoimage {
         </menu>
         <section class="principal">
           <menu class="vertical">
-            <div class="icone-wrapper" @click="mode = 'ajout:pret'">
+            <div class="icone-wrapper invert" :class="{actif: mode.match(/ajout/)}" @click="mode = 'ajout:pret'">
               <img src="${Ellipse}">
             </div>
-            <div class="icone-wrapper">
+            <div class="icone-wrapper invert">
               <img src="${Image}">
+            </div>
+            <div class="icone-wrapper" :class="{flash: mode == 'session:active'}" @click="toggle_session">
+              <img src="${Record}">
             </div>
           </menu>
           <div class="app-fonoimage" ref="application_fonoimage">
